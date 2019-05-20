40ac68884913fafc8a910d4da3c32923baead7e2
“c2kaka <c2kaka@126.com>
2019-05-20 21:38:32 +0800

    增加橡皮擦功能

---
+++ main.js
@@ -0,0 +1,78 @@
+let myCanvas=document.getElementById("canvas");
+let context=myCanvas.getContext("2d");
+let using=false;
+let lastPosition={x:undefined,y:undefined};
+setCanvasSize();
+
+window.onresize=function () {
+    setCanvasSize();
+}
+
+function setCanvasSize() {
+    myCanvas.width=document.documentElement.clientWidth;
+    myCanvas.height=document.documentElement.clientHeight;
+}
+
+function drawCircle(x,y) {
+    context.beginPath();
+    context.arc(x,y,5,0,Math.PI*2);
+    context.fillStyle="red";
+    context.fill();
+}
+
+function drawLine(lastP,currentP)
+{
+    context.beginPath();
+    context.strokeStyle = "red";
+    context.lineWidth = 10;
+    context.moveTo(lastP.x, lastP.y);
+    context.lineTo(currentP.x, currentP.y);
+    context.stroke();
+}
+
+myCanvas.onmousedown=function (e) {
+    using=true;
+    const x=e.clientX;
+    const y=e.clientY;
+    if(!eraserEnabled){
+        drawCircle(x,y);
+        lastPosition.x=x;
+        lastPosition.y=y;
+    } else {
+      context.clearRect(x-5,y-5,20,20);
+    }
+};
+myCanvas.onmousemove=function (e) {
+    if(using){
+        const x=e.clientX;
+        const y=e.clientY;
+        if(!eraserEnabled){
+            drawCircle(x,y);
+            const currentPosition={x:x,y:y};
+            drawLine(lastPosition,currentPosition);
+            lastPosition=currentPosition;
+        }else{
+            context.clearRect(x-5,y-5,20,20);
+        }
+    }
+};
+
+myCanvas.onmouseup=function (e) {
+    using=false;
+};
+
+let divActions=document.getElementById("actions");
+let eraser=document.getElementById("eraser");
+let brush=document.getElementById("brush");
+let eraserEnabled=false;
+eraser.onclick=function (e) {
+    divActions.classList.add("eraserEnable");
+    eraserEnabled=true;
+};
+
+brush.onclick=function (e) {
+    divActions.classList.remove("eraserEnable");
+    eraserEnabled=false;
+}
+
+
